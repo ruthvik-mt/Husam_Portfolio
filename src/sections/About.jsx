80203ee7b582ce1bc85793a0807dca80bc5d8f33
Huzaif Ahmed <dev.huzaif@gmail.com>
2025-04-09 17:21:19 +0530

    final-commit

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const [hasCopied, setHasCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText('team.devxora@');
+    navigator.clipboard.writeText('team.devxora@gmail');
     setHasCopied(true);
 
     setTimeout(() => {
