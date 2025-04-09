47d421d7cabf0694697144a2f4796408ad710987
Huzaif Ahmed <dev.huzaif@gmail.com>
2025-04-09 16:18:59 +0530

    email-js-done!!

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -22,8 +22,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        // import.meta.env.YOUR EMAILJS_SERVICE_ID,
-        // import.meta.env. YOUR EMAILJS TEMPLETE_ID,
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: 'your name',
